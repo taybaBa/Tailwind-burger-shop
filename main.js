6531aed5e6fa05cec4abc890722672c81a23bb85
Tayyba Bashir <taybabashir20@gmail.com>
2024-07-18 08:59:01 +0500

    added tabs and review section

--- main.js
+++ main.js
@@ -16,3 +16,49 @@ navMenu.classList.remove('hidden')
 closeIcon.addEventListener("click",() => {
     navMenu.classList.add('hidden')
     })
+    // =====================Tabs===============
+    const tabs = document.querySelectorAll(".tabs_wrap ul li");
+    const all = document.querySelectorAll(".item_wrap");
+    const foods = document.querySelectorAll(".food");
+    const beverages = document.querySelectorAll(".beverage");
+    const snacks = document.querySelectorAll(".snack");
+   
+
+
+    tabs.forEach(tab => {
+        tab.addEventListener("click", () => {
+            tabs.forEach(tab => {
+                tab.classList.remove("active");
+            });
+    
+            tab.classList.add("active");
+    
+            const tabval = tab.getAttribute("data-tabs");
+            console.log(tabval);
+            all.forEach(item => {
+                item.style.display = 'none';
+            });
+
+            if(tabval == 'food'){
+                foods.forEach(item => {
+                    item.style.display = 'block';
+                });
+            }
+            else if(tabval == 'snack'){
+                snacks.forEach(item => {
+                    item.style.display = 'block';
+                });
+            }
+            else if(tabval == 'beverage'){
+                beverages.forEach(item => {
+                    item.style.display = 'block';
+                });
+            }
+           else{
+            all.forEach(item => {
+                item.style.display = 'block';
+            });
+            }
+        });
+    });
+    
